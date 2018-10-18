aac5b6c235223894bf52b84924f7ad3306e162fb
alastaira <alastair@a3uk.com>
2018-10-18 16:07:36 +0100

    Add files via upload

---
+++ SerialKeyMapper/SerialKeyMapper.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Playful.Technology
+{
+    partial class SerialKeyMapper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SerialKeyMapper));
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.chkOnOff = new System.Windows.Forms.CheckBox();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkStripWhitespace = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboPort
+            // 
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Location = new System.Drawing.Point(135, 18);
+            this.cboPort.Margin = new System.Windows.Forms.Padding(4);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(141, 24);
+            this.cboPort.TabIndex = 0;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(135, 50);
+            this.cboBaudRate.Margin = new System.Windows.Forms.Padding(4);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(141, 24);
+            this.cboBaudRate.TabIndex = 1;
+            // 
+            // chkOnOff
+            // 
+            this.chkOnOff.AutoSize = true;
+            this.chkOnOff.Location = new System.Drawing.Point(135, 111);
+            this.chkOnOff.Margin = new System.Windows.Forms.Padding(4);
+            this.chkOnOff.Name = "chkOnOff";
+            this.chkOnOff.Size = new System.Drawing.Size(82, 21);
+            this.chkOnOff.TabIndex = 2;
+            this.chkOnOff.Text = "Enabled";
+            this.chkOnOff.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Baud Rate";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // chkStripWhitespace
+            // 
+            this.chkStripWhitespace.AutoSize = true;
+            this.chkStripWhitespace.Location = new System.Drawing.Point(135, 82);
+            this.chkStripWhitespace.Margin = new System.Windows.Forms.Padding(4);
+            this.chkStripWhitespace.Name = "chkStripWhitespace";
+            this.chkStripWhitespace.Size = new System.Drawing.Size(145, 21);
+            this.chkStripWhitespace.TabIndex = 6;
+            this.chkStripWhitespace.Text = "Strip Whitespace?";
+            this.chkStripWhitespace.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Playful.Technology.Properties.Resources.PlayfulTechLogo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 66);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SerialKeyMapper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 161);
+            this.Controls.Add(this.chkStripWhitespace);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkOnOff);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SerialKeyMapper";
+            this.Text = "Serial Keystroke Mapper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.CheckBox chkOnOff;
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox chkStripWhitespace;
+    }
+}
+
---
+++ SerialKeyMapper/SerialKeyMapper.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace Playful.Technology
+{
+    /// <summary>
+    ///  Listens for a line of delimited data on the specified serial port (e.g. as sent from
+    ///  the Arduino Serial.println() method), and converts this to a sequence of keystrokes 
+    ///  which are sent to the currently active application
+    /// </summary>
+    public partial class SerialKeyMapper : Form
+    {
+        // List of available baud rates at which serial port can be connected
+        private int[] bauds = {9600, 14400, 19200, 38400, 57600, 115200};
+        // Is the application currently listening to the serial port
+        private bool isRunning = false;
+        // The port in use
+        private SerialPort port = null;
+
+        public SerialKeyMapper()
+        {
+            InitializeComponent();
+
+            // Populate combo boxes and other form elements with appropriate values
+            Populate();
+
+            // Initialise the serial port with some common values
+            port = new SerialPort();
+            port.ReadTimeout = 500;
+            port.WriteTimeout = 500;
+
+            // Arduino println sends a string terminated with \r\n 
+            port.NewLine = "\r\n";
+
+            // Attach event handlers
+            this.chkOnOff.CheckedChanged += new EventHandler(ChkOnOff_CheckedChanged_Handler);
+            this.FormClosing += new FormClosingEventHandler(FormClosing_Handler);
+            this.bgWorker.DoWork += new DoWorkEventHandler(BackgroundWork_Handler);
+            this.bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(StartStop);
+        }
+
+        // Called when the application is closed
+        void FormClosing_Handler(object sender, FormClosingEventArgs e)
+        {
+            if (port.IsOpen) {
+                port.Close();
+            }
+            chkOnOff.Checked = false;
+        }
+
+        // Called when the option for enabling/disabling the application is toggled
+        void ChkOnOff_CheckedChanged_Handler(object sender, EventArgs e) {
+            if (isRunning) {
+                isRunning = false;
+            }
+            else {
+                StartStop(null, null);
+            }
+
+            ToggleFormControlsActive(!chkOnOff.Checked);
+        }
+
+        // Sets whether form controls are enabled or not
+        private void ToggleFormControlsActive(bool enable) {
+            cboBaudRate.Enabled = cboPort.Enabled = enable;
+        }
+
+        // Populates the various form controls with options
+        private void Populate() {
+            // COM Port combo
+            cboPort.Items.Clear();
+            string[] portList = SerialPort.GetPortNames();
+            foreach (string port in portList) {
+                // Some guides suggest that to address COM ports greater than "COM9", you must use NT address format "\\.\COM10", "\\.\COM11" etc.
+                // This could be achieved using the following, but doesn't actually appear to be necessary:
+                // string portNTformat = string.Concat(@"\\.\", port);
+                string portNTformat = port; 
+                cboPort.Items.Add(portNTformat);
+            }
+
+            // Baud rate combo
+            cboBaudRate.DataSource = bauds;
+            cboBaudRate.SelectedIndex = 0;
+        }
+
+        private void StartStop(object sender, RunWorkerCompletedEventArgs args) {
+            ToggleFormControlsActive(!chkOnOff.Checked);
+
+            // If port is already open, close it
+            if (port.IsOpen) {
+                port.Close();
+            }
+           
+            // If we've just started listening
+            if (chkOnOff.Checked)  {
+                if (cboPort.SelectedItem != null) {
+                    try {
+                        port = new SerialPort((string)cboPort.SelectedItem, (int)cboBaudRate.SelectedItem);
+                        port.Open();
+                        isRunning = true;
+                        bgWorker.RunWorkerAsync();
+                    }
+                    catch(Exception e) {
+                        isRunning = false;
+                        chkOnOff.Checked = false;
+                        ToggleFormControlsActive(true);
+                    }
+                }
+            }
+        }
+
+        // Background handler actually does the work of listening to the serial
+        // port and converting to keystrokes
+        private void BackgroundWork_Handler(object sender, DoWorkEventArgs args) {
+            while (isRunning) {
+                try {
+                    var line = port.ReadLine();
+                    if (chkStripWhitespace.Checked) {
+                        // Strip any whitespace, newline, carriage returns etc.
+                        line = Regex.Replace(line, @"\s+", string.Empty);
+                    }
+                    SendKeys.SendWait(line);
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+    }
+}
